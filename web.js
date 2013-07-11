876849fb9df6aa5789c3b743686d3996250aa0d5
Ubuntu <ubuntu@ip-172-31-29-13.us-west-2.compute.internal>
2013-07-11 02:42:14 +0000

    Updated5 web.js

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ var infile = 'index.html';
 var buffer = fs.readFileSync(infile);
 
 app.get('/', function(request, response) {
- response.send(Buffer);
+ response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
