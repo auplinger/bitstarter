59e19f6cc6067847103bdace89e7b6686f888cfa
Ubuntu <ubuntu@ip-172-31-29-13.us-west-2.compute.internal>
2013-07-21 23:27:30 +0000

    responsive design

--- web.js
+++ web.js
@@ -1,8 +1,8 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var fs = require('fs');
 var infile = 'index.html';
 
 var buffer = fs.readFileSync(infile);
@@ -11,7 +11,7 @@ app.get('/', function(request, response) {
     response.send(buffer.toString('utf-8'));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
